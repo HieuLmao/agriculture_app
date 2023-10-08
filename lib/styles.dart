d6dfbe7e961feefb2784ffda53ec5531caaed91e
hieu nguyen <hieumi0772@gmail.com>
2023-10-08 09:43:41 +0700

    size

--- lib/styles.dart
+++ lib/styles.dart
@@ -2,3 +2,11 @@ import 'package:flutter/material.dart';
 
 var whiteColor = const Color(0xffffffff);
 var blackColor = const Color(0xff000000);
+
+// size
+
+var small = 10.0;
+var sx = 15.0;
+var medium = 20.0;
+var mx = 25.0;
+var large = 30.0;
