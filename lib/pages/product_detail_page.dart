f573747227da9b840ee74ab68f57750934efa848
hieu nguyen <hieumi0772@gmail.com>
2023-10-08 23:25:10 +0700

    add remove

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -1,8 +1,10 @@
 import 'package:agriculture_app/data/products.dart';
 import 'package:agriculture_app/models/product.dart';
+import 'package:agriculture_app/providers/product_provider.dart';
 import 'package:agriculture_app/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailPage extends StatefulWidget {
   static String routesName = 'productDetailPage';
@@ -14,8 +16,24 @@ class ProductDetailPage extends StatefulWidget {
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
+  int quantity = 0;
+  void incrementQuantity() {
+    setState(() {
+      quantity++;
+    });
+  }
+
+  void decrementQuantity() {
+    setState(() {
+      if (quantity > 0) {
+        quantity--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final response = context.read<ProductsProvider>();
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -91,13 +109,16 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                 Row(
                   children: [
                     IconButton.filled(
-                      onPressed: () {},
+                      onPressed: () {
+                        decrementQuantity();
+                      },
                       icon: const Icon(Icons.remove),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                    Container(
+                      width: 80,
+                      alignment: Alignment.center,
                       child: Text(
-                        '5${widget.product.unit}',
+                        '$quantity ${widget.product.unit}',
                         style: Theme.of(context)
                             .textTheme
                             .titleMedium!
@@ -105,7 +126,9 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
                       ),
                     ),
                     IconButton.filled(
-                      onPressed: () {},
+                      onPressed: () {
+                        incrementQuantity();
+                      },
                       icon: const Icon(Icons.add),
                     ),
                   ],
@@ -162,7 +185,9 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
             ),
             SizedBox(height: medium),
             FilledButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                response.addCartItem(widget.product);
+              },
               icon: const Icon(IconlyLight.bag_2),
               label: const Text('Add to cart'),
             ),
