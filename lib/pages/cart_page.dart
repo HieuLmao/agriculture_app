43808313baed25abc0d845dcf78843f9498a4c0d
hieu nguyen <hieumi0772@gmail.com>
2023-10-08 23:25:24 +0700

    cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,24 @@
+import 'package:agriculture_app/providers/product_provider.dart';
+import 'package:agriculture_app/widgets/cart_item.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ProductsProvider>(
+      builder: (context, value, child) => value.cartList.isEmpty
+          ? SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: Image.asset('assets/images/empty.gif'),
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) =>
+                  CartItem(product: value.cartList[index]),
+              itemCount: value.cartList.length,
+            ),
+    );
   }
 }
