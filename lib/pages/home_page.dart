d1071d155646a23163761a9fffb0977d55dabcfd
hieu nguyen <hieumi0772@gmail.com>
2023-10-08 09:44:14 +0700

    btom bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ import 'package:agriculture_app/pages/service_page.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 import 'package:badges/badges.dart' as badges;
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,71 +25,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
-      appBar: AppBar(
-        centerTitle: false,
-        leading: const Icon(Icons.menu),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Hi HieuNguyen !!',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Text(
-              'Enjoy our service',
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
+        drawer: const Drawer(),
+        appBar: AppBar(
+          centerTitle: false,
+          leading: const Icon(Icons.menu),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hi HieuNguyen !!',
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              Text(
+                'Enjoy our service',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: IconButton.filledTonal(
+                onPressed: () {},
+                icon: badges.Badge(
+                  position: badges.BadgePosition.topEnd(top: -15, end: -12),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.green,
+                  ),
+                  badgeContent: const Text('2'),
+                  child: const Icon(IconlyBroken.notification),
+                ),
+              ),
+            )
           ],
         ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: IconButton.filledTonal(
-              onPressed: () {},
-              icon: badges.Badge(
-                position: badges.BadgePosition.topEnd(top: -15, end: -12),
-                badgeStyle: const badges.BadgeStyle(
-                  badgeColor: Colors.green,
-                ),
-                badgeContent: const Text('2'),
-                child: const Icon(IconlyBroken.notification),
+        body: pageList[pageIndex],
+        bottomNavigationBar: SalomonBottomBar(
+            itemPadding: const EdgeInsets.all(15),
+            currentIndex: pageIndex,
+            onTap: (p0) {
+              setState(() {
+                pageIndex = p0;
+              });
+            },
+            items: [
+              SalomonBottomBarItem(
+                icon: const Icon(IconlyLight.home),
+                title: const Text('Home'),
               ),
-            ),
-          )
-        ],
-      ),
-      body: pageList[pageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: pageIndex,
-          onTap: (value) {
-            setState(() {
-              pageIndex = value;
-            });
-          },
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(IconlyLight.home),
-              activeIcon: Icon(IconlyBold.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconlyLight.call),
-              activeIcon: Icon(IconlyBold.call),
-              label: 'Service',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconlyLight.buy),
-              activeIcon: Icon(IconlyBold.buy),
-              label: 'Cart',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(IconlyLight.profile),
-              activeIcon: Icon(IconlyBold.profile),
-              label: 'Profile',
-            ),
-          ]),
-    );
+              SalomonBottomBarItem(
+                icon: const Icon(IconlyLight.call),
+                title: const Text('Services'),
+              ),
+              SalomonBottomBarItem(
+                icon: const Icon(IconlyLight.buy),
+                title: const Text('Cart'),
+              ),
+              SalomonBottomBarItem(
+                icon: const Icon(IconlyLight.profile),
+                title: const Text('Profile'),
+              ),
+            ]));
   }
 }
